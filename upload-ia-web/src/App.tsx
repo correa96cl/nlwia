3c97359a2d2e9300d10adbbc929349fd310f8012
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-09-20 21:54:14 -0300

    Primeiro commit NLW

---
+++ upload-ia-web/src/App.tsx
@@ -0,0 +1,27 @@
+import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+
+export function App() {
+
+  return (
+
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
